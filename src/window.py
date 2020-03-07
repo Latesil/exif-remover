0b7441413a32df2287cc2fc656276a9379abfd42
Latesil <skipastas@gmail.com>
2020-03-07 13:07:43 +0300

    add basic interface

--- src/window.py
+++ src/window.py
@@ -15,15 +15,48 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+import gi
+gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
-#test
 
 
 @Gtk.Template(resource_path='/com/gitlab/Latesil/exif-remover/window.ui')
 class ExifRemoverWindow(Gtk.ApplicationWindow):
     __gtype_name__ = 'ExifRemoverWindow'
 
-    label = Gtk.Template.Child()
+    add_button = Gtk.Template.Child()
+    rename_checkbox = Gtk.Template.Child()
+    preferences_button = Gtk.Template.Child()
+    about_button = Gtk.Template.Child()
+    rename_entry = Gtk.Template.Child()
+    main_info_label = Gtk.Template.Child()
+    about_dialog = Gtk.Template.Child()
+    preferences_dialog = Gtk.Template.Child()
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+
+    @Gtk.Template.Callback()
+    def on_add_button_clicked(self, button):
+        print('on_add_button_clicked')
+
+    @Gtk.Template.Callback()
+    def on_rename_checkbox_toggled(self, box):
+        print('on_rename_checkbox_toggled')
+
+    @Gtk.Template.Callback()
+    def on_preferences_button_clicked(self, button):
+        preferences = self.preferences_dialog
+        preferences.run()
+        preferences.destroy()
+
+    @Gtk.Template.Callback()
+    def on_about_button_clicked(self, button):
+        about = self.about_dialog
+        about.run()
+        about.destroy()
+
+    @Gtk.Template.Callback()
+    def on_rename_entry_changed(self, entry):
+        print('on_rename_entry_changed')
+        
