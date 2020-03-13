e050250b2faf598dabe96066af34ad60e00a626f
Latesil <skipastas@gmail.com>
2020-03-14 01:01:58 +0300

    separate func

--- src/folder_box.py
+++ src/folder_box.py
@@ -1,7 +1,9 @@
 import gi
 gi.require_version('Gtk', '3.0')
 gi.require_version('GExiv2', '0.10')
-from gi.repository import Gtk, Gio, GLib, GExiv2, GObject
+from gi.repository import Gtk, Gio, GLib, GExiv2
+
+from .helpers import get_files_and_folders, clear_metadata
 
 @Gtk.Template(resource_path='/com/gitlab/Latesil/exif-remover/folder_box.ui')
 class FolderBox(Gtk.Box):
@@ -66,20 +68,8 @@ class FolderBox(Gtk.Box):
                     continue
 
                 #print('%s: %s in file: %s (code: %s)' % (err.domain, err.message, f, err.code))
-            exif = GExiv2.Metadata()
-            exif.open_path(f_in.get_path())
 
-            exif.clear_comment()
-            exif.clear_exif()
-            exif.clear_iptc()
-            exif.clear_xmp()
-            exif.delete_gps_info()
-
-            #TODO
-            #exif.erase_exif_thumbnail()
-
-            exif.clear()
-            exif.save_file(f_out.get_path())
+            clear_metadata(f_in, f_out)
             n += 1
 
         if self.skipped:
@@ -92,29 +82,5 @@ class FolderBox(Gtk.Box):
         label.set_text('Done')
         infobar.props.revealed = True
 
-
-
-
-def get_files_and_folders(folder, absolute_folders_paths=True):
-    folder_list = []
-    files_list = []
-
-    path = Gio.File.new_for_path(folder)
-    enumerator = path.enumerate_children(Gio.FILE_ATTRIBUTE_STANDARD_NAME, Gio.FileQueryInfoFlags.NONE)
-    info = enumerator.next_file()
-    while info is not None:
-        if info.get_file_type() == Gio.FileType.DIRECTORY:
-            if absolute_folders_paths:
-                folder_path = path.get_path() + '/' + info.get_name()
-            else:
-                folder_path = info.get_name()
-            folder_list.append(folder_path)
-            info = enumerator.next_file()
-        else:
-            abs_path = path.get_path() + '/' + info.get_name()
-            files_list.append(abs_path)
-            info = enumerator.next_file()
-
-    return folder_list, files_list
 
     
