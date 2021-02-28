6c45020809ec2492a72941c3e759f7f560a10d07
Latesil <vihilantes@gmail.com>
2021-02-28 22:30:46 +0300

    add basic folder template

--- src/folders_view.py
+++ src/folders_view.py
@@ -1,11 +1,20 @@
 import gi
+
 gi.require_version('Gtk', '3.0')
 gi.require_version('Handy', '1')
 from gi.repository import Gtk, Gio, GLib, Handy
+from .exif_folder import ExifFolder
+
 
 @Gtk.Template(resource_path="/com/github/Latesil/exif-remover/FoldersView.ui")
 class FoldersView(Gtk.Stack):
     __gtype_name__ = "FoldersView"
 
+    folders_view_container = Gtk.Template.Child()
+
     def __init__(self):
         super().__init__()
+
+        exif_folder = ExifFolder()
+
+        self.folders_view_container.add(exif_folder)
