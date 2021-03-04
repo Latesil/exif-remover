6ad9920993c05f9c278fa142c41e82461de37cc4
Latesil <vihilantes@gmail.com>
2021-03-04 09:57:47 +0300

    add enter and leave events

--- src/exif_folder.py
+++ src/exif_folder.py
@@ -2,7 +2,7 @@ import gi
 
 gi.require_version('Gtk', '3.0')
 gi.require_version('Handy', '1')
-from gi.repository import Gtk, Gio, GLib, Handy
+from gi.repository import Gtk, Gio, GLib, Handy, Gdk
 
 
 @Gtk.Template(resource_path="/com/github/Latesil/exif-remover/ui/ExifFolder.ui")
@@ -22,6 +22,14 @@ class ExifFolder(Gtk.Box):
     @Gtk.Template.Callback()
     def on_image_event_box_clicked(self, event, widget):
         GLib.spawn_async(['/usr/bin/xdg-open', self.path])
+
+    @Gtk.Template.Callback()
+    def on_enter_notify_event(self, event, widget):
+        print('enter')
+
+    @Gtk.Template.Callback()
+    def on_leave_notify_event(self, event, widget):
+        print('leave')
 
     @Gtk.Template.Callback()
     def on_close_exif_folder_clicked(self, button):
