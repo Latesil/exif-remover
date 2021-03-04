a8f32f7efb5a1e8dfc79dc60ac37d2112eb82fb4
Latesil <vihilantes@gmail.com>
2021-03-04 16:00:51 +0300

    add sync preview

--- src/exif_file.py
+++ src/exif_file.py
@@ -3,7 +3,7 @@ from locale import gettext as _
 
 gi.require_version('Gtk', '3.0')
 gi.require_version('Handy', '1')
-from gi.repository import Gtk, Gio, GLib, Handy, Gdk
+from gi.repository import Gtk, Gio, GLib, Handy, GdkPixbuf
 
 
 @Gtk.Template(resource_path="/com/github/Latesil/exif-remover/ui/ExifFile.ui")
@@ -11,9 +11,12 @@ class ExifFile(Gtk.FlowBoxChild):
     __gtype_name__ = "ExifFile"
 
     exif_file_label = Gtk.Template.Child()
+    exif_file_image = Gtk.Template.Child()
 
     def __init__(self, path):
         super().__init__()
         self.path = path
 
         self.exif_file_label.props.label = GLib.basename(path)
+        self.image = GdkPixbuf.Pixbuf.new_from_file_at_scale(self.path, 120, -1, True)
+        self.exif_file_image.set_from_pixbuf(self.image)
